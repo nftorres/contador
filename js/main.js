7e777282c653ac7409325e94c7d0b5957afcebac
Fabián Torres <nftorresc@gmail.com>
2024-03-09 15:57:50 -0500

    Refactorización

--- js/main.js
+++ js/main.js
@@ -5,19 +5,29 @@ const btn_resetear = document.querySelector("#resetear");
 const btn_incrementar = document.querySelector("#incrementar");
 
 btn_decrementar.addEventListener("click", () => {
+    decrementarContador();
+});
+
+btn_resetear.addEventListener("click", () => {
+    resetearContador();
+});
+
+btn_incrementar.addEventListener("click", () => {
+    incrementarContador();
+});
+
+function decrementarContador() {
     if (contador.textContent !== "0") {
         contador.textContent = eval(contador.textContent + "- 1");
     } else {
         contador.textContent = "0";
     }
-});
+}
 
-btn_resetear.addEventListener("click", () => {
+function resetearContador() {
     contador.textContent = "0";
-});
+}
 
-btn_incrementar.addEventListener("click", () => {
+function incrementarContador() {
     contador.textContent = eval(contador.textContent + "+ 1");
-});
-
-contador.textContent = cuenta;
+}
